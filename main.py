a73bc45e02cc7d62b9a6029a393ab3578d7efe29
ShinobiPy <71345519+badbuh@users.noreply.github.com>
2021-10-11 11:20:33 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,22 @@
+###Exercise 1: Print First 10 natural numbers using while loop
+# i = 0
+# while i <= 10:
+#     print(i)
+#     i += 1
+###Exercise 2: Print the following pattern
+#rows = 5
+#
+# for i in range(rows+1):
+#     for j in range(i):
+#         j += 1
+#         print(j, end='')
+#     print('')
+###Exercise 3: Calculate the sum of all numbers from 1 to a given number
+# num = int(input("Enter your number: "))
+# suma = 0
+# for i in range(num):
+#     i += 1
+#     suma += i
+#
+# print(suma)
+###Exercise 4: Write a program to print multiplication table of a given number
